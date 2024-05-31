0a6c528fa4b532626939824ac33511b7f1a694c9
Akshatha1967 <akshathakadam2@gmail.com>
2024-05-31 15:52:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Fragment } from 'react';
 import Header from './components/layout/header';
 import MealsSummary from './components/Meals/MealsSummary';
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
     <Fragment className="App">
+      
       <Header/>
+      <Cart/>
       <MealsSummary/>
       <Meals/>
+    
     </Fragment>
   );
 }
