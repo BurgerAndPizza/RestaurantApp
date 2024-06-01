3887e6b77714f9e19c21534d9802fbb8c1489f8e
Akshatha1967 <akshathakadam2@gmail.com>
2024-06-01 16:39:29 +0530

    first commit

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
         <Fragment>
             <header className= {classes.header}>
                 <h1>React Meals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart}/>
             </header>
             <div className={classes['main-image']}>
                 <img src="https://honeybeemeals.com/cdn/shop/files/honey-bee-meals-toronto-gta-meal-delivery_2000x.jpg?v=1708615186" alt="A table full of delecious food!"/>
--- src/components/layout/headerCartButton.js
+++ src/components/layout/headerCartButton.js
@@ -3,7 +3,7 @@ import React from "react";
 import CartIcon from "../Cart/cartIcon";
 const headerCartButton = (props) =>{
     return(
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick} >
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
