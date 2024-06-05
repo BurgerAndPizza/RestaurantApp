808b5e6eafd1b3baa44ea17dee8ca6aae4d0d2f8
Akshatha1967 <akshathakadam2@gmail.com>
2024-06-05 16:14:52 +0530

    first commit

--- src/components/layout/headerCartButton.js
+++ src/components/layout/headerCartButton.js
@@ -1,16 +1,20 @@
+import { useContext } from "react";
 import classes from "./headerCartButton.module.css";
 import React from "react";
 import CartIcon from "../Cart/cartIcon";
+import CartContext from "../../store/cart-context";
 const headerCartButton = (props) =>{
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount
+    }, 0);
     return(
         <button className={classes.button} onClick={props.onClick} >
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>
-                3
-            </span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 }
---
+++ src/store/CartProvider.js
@@ -0,0 +1,15 @@
+import CartContext from "./cart-context";
+const CartProvider = props =>{
+    const addItemToCartHandler = items =>{};
+    const removeItemFromCartHandler = id =>{};
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    };
+    return<CartContext value={cartContext}> 
+
+    </CartContext>
+};
+export default CartProvider;
